378a375abffff9cf519e5adf63d84cc4c94a1fc4
Vasyl Boychuk <vasyl.boychuk@panion.org>
2024-07-03 20:51:21 +0300

    pg and .env

--- README.md
+++ README.md
@@ -4,6 +4,19 @@
 $ pnpm install
 ```
 
+## up pgsql via docker
+docker run --name docker-postgres -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=mydatabase -v ./pgdata:/var/lib/postgresql/data -p 5433:5432 -d postgres
+
+docker stop docker-postgres
+docker rm docker-postgres
+
+psql -h localhost -p 5433 -U postgres -W
+
+\c mydatabase
+\dt
+
+\q
+
 ## Running the app
 
 ```bash
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true, // Note: set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
